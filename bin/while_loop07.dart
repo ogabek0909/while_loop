764b98b267f93cbf6e0539e5e7cc4b271dc9d14c
ogabek <norpulatovogabek42@gmail.com>
2022-07-07 21:57:26 +0500

    dsf

--- bin/while_loop07.dart
+++ bin/while_loop07.dart
@@ -3,4 +3,23 @@
   Return odd and even numbers up to "a", whichever is smaller
   
 */
-void main() {}
+int func(int a) {
+  int s = 0;
+  int y = 0;
+  int q = 0;
+  while (q < a) {
+    q += 1;
+    if (q % 2 == 1) {
+      s = s + q;
+    } else
+      y += q;
+  }
+  if (s < y) {
+    return s;
+  } else
+    return y;
+}
+
+void main() {
+  print(func(10));
+}
