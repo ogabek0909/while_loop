7ece3d94b34ff563bbce33bc33f2d63e16cbfac1
ogabek <norpulatovogabek42@gmail.com>
2022-07-11 21:58:20 +0500

    adfs

--- bin/while_loop10.dart
+++ bin/while_loop10.dart
@@ -8,22 +8,24 @@
 */
 
 String func(a) {
-  int q = 2;
+  int q = 0;
   int w = 0;
   String s = 'tub son emas';
   String r = 'tub son';
   String g = '';
-  while (q < a) {
+  while (q <= a) {
     q++;
-    if (a % q == 0 || a % 2 == 0) {
-      q++;
-      return s;
+    if (a % q == 0) {
+      w++;
+    }
+    if (w == 2) {
+      g = r;
     } else
-      return r;
+      g = s;
   }
-  return '';
+  return g;
 }
 
 void main() {
-  print(func(5));
+  print(func(545387));
 }
